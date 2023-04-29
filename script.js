4785857ff84330c6a43a4b24568e340e1aeb1410
luckysevchenko <badboy138713@gmail.com>
2023-04-29 20:14:57 +0300

    Js hw1

---
+++ script.js
@@ -0,0 +1,54 @@
+/задания 1/
+let a =10;
+alert(a);
+a = 20;
+ /задания 2/
+let year = 2007;
+alert(year);
+ /задания 3/
+let namejs = Brendan Eich
+alert(namejs)
+/задания 4/
+let b = 10;
+let c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+/задания 5/
+let result = 2**5;
+alert(reult);
+/задания 6/
+let d =9;
+let e =2;
+alert(d % e)
+/задания 7/
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+/задания 8/
+age = prompt(`Сколько вам лет?`);
+alert(age);
+/задания 9/
+const user = {
+    name:`Andrey`,
+    age:35,
+    isAdmin:true,
+}
+/задания 9.1/
+user[`city of residence`] = `Saint-Peterburg`
+/задания 9.2/
+user.age = 36;
+/задания 9.3/
+delet user [`city of residence`];
+/задания 9.4/
+let info = prompt(`Какую информацию хотите узнать о пользователе`);
+alert(user[info]);
+/задания 10/
+let userName = prompt (`Как Вас зовут?`)
+alert(Привкт,${userName});
