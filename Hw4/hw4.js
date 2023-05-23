862695eaf9ef4cb8edaf009938f2c51d84ac529d
LuckyShevchenko <badman1387@gmail.com>
2023-05-24 00:20:10 +0300

    hw4 new

--- Hw4/hw4.js
+++ Hw4/hw4.js
@@ -3,12 +3,15 @@
 for(let say = 0,say = 2,say++);
 console.log( say );
 //2 задания
+
 for(let i = 1,i < 6,i++);
 console.log(i);
 //3 задания
+
 for(let i = 7,i <= 22,i++);
 console.log(i);
 //4 задания
+
 const obj{
   Коля:200,
   Вася:300,
@@ -18,6 +21,7 @@ for(let worker in obj){
   consol.log(`${worker}-зарплата,${obj[worker]}`)
 };
 //5 задания
+
 let n = 1000;
 let num = 1;
 do{
@@ -26,6 +30,7 @@ do{
 }while(n > 50);
 console.log(`получилось число ${n} при количестве интераций ${--num}`);
 //6 задания
+
 let friday = 5;
 for(let i = 1, i <= 31, i++){
   if (i === friday){
